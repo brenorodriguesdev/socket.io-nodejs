fc04d37236036bdb6352d20b23ae2a03f933f137
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-06-30 20:07:15 -0300

    feat: initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const httpServer = http_1.createServer();
+const io = new socket_io_1.Server(httpServer, {
+// ...
+});
+io.on("connection", (socket) => {
+    // ...
+});
+httpServer.listen(3500);
+//# sourceMappingURL=index.js.map
