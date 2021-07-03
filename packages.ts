29d6c97a5d4890668399edd89b2b2cd0c8c1c5a1
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-07-02 22:10:18 -0300

    feat: add onReceivePrivateMessage package

--- packages.ts
+++ packages.ts
@@ -2,6 +2,11 @@ import { Server } from 'socket.io'
 
 var users: User[] = []
 
+export type MessagePackage = {
+    id: string
+    message: string
+}
+
 export function onConnect(user: User, server: Server) {
     users.push(user)
     server.emit('sendPlayers', users)
@@ -10,4 +15,8 @@ export function onConnect(user: User, server: Server) {
 export function onDisconnect(id: string, server: Server) {
     users = users.filter(user => user.id != id)
     server.emit('sendPlayers', users)
+}
+
+export function onReceivePrivateMessage(messagePackage: MessagePackage, server: Server) {
+    server.to(messagePackage.id).emit('onReceivePrivateMessage', messagePackage.message)
 }
--- socket.ts
+++ socket.ts
@@ -1,32 +1,19 @@
 import { Server } from 'socket.io'
 import { httpServer } from './http'
-import { onConnect, onDisconnect } from './packages';
+import { MessagePackage, onConnect, onDisconnect, onReceivePrivateMessage } from './packages';
 
-export const io = new Server(httpServer, {
-    cors: {
-        origin: '*'
-    }
-});
+export const io = new Server(httpServer, { cors: { origin: '*' } });
 
 io.on("connection", (socket) => {
 
-
     socket.emit('onConnect')
 
-    socket.on('onConnect', (username: string) => {
+    socket.on('onConnect', (username: string) => onConnect({ id: socket.id, username, }, io))
 
-        onConnect({
-            id: socket.id,
-            username,
-        }, io)
+    socket.on('disconnect', () => onDisconnect(socket.id, io))
 
-    })
+    socket.on('onReceivePrivateMessage', (messagePackage: MessagePackage) => onReceivePrivateMessage(messagePackage, io))
 
-    socket.on('disconnect', function () {
-
-        onDisconnect(socket.id, io)
-
-    });
 });
 
 httpServer.listen(3500)
